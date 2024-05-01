40267d460e9adcb3ff34e0ab94d641b14a37ab7a
umeramin218 <160252422+umeramin218@users.noreply.github.com>
2024-05-01 18:59:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var firstName = "umer";
+var lastname = "amin";
+var fullname = firstName + ' ' + lastname;
+console.log(fullname);
+var nameOne = "usman";
+var nameTwo = "amin";
+var compeleteName = "".concat(nameOne, " ").concat(nameTwo);
+console.log(compeleteName);
---
+++ main.ts
@@ -0,0 +1,12 @@
+let firstName: string = "umer";
+let lastname: string = "amin";
+let fullname: string = firstName +' '+ lastname;
+console.log(fullname);
+
+let nameOne : string = "usman";
+let nameTwo : string= "amin";
+let compeleteName: string = `${nameOne} ${nameTwo}`;
+console.log(compeleteName);
+
+
+
